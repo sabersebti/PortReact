1108179bd105f5f012f9e9305d4450ffad9f888e
saber sebti <sebti.saber1@gmail.com>
2023-03-09 10:42:05 +0000

    added the hashrouter

--- port-react/src/App.js
+++ port-react/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import NavTabs from './components/NavTabs';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
